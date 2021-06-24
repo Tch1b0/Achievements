0d0685ba7dac760c9763c2f2fa3e9cfbc8da1b5e
Tch1b0 <Jpour3006@gmail.com>
2021-06-24 20:14:28 +0200

    show number of accomplished achievements

--- src/Panel/AchievementPanel.ts
+++ src/Panel/AchievementPanel.ts
@@ -1,6 +1,6 @@
 import * as vscode from "vscode";
+import { accomplishedAchievements } from "../achievements/accomplishedAchievements";
 import { Achievement } from "../achievements/Achievement";
-import { getAchievements } from "../achievements/achievements";
 import { getNonce } from "./getNonce";
 
 export class AchievementPanel {
@@ -187,7 +187,6 @@ export class AchievementPanel {
                 -->
                 <meta http-equiv="Content-Security-Policy" content="img-src https: data:; style-src 'unsafe-inline' ${webview.cspSource
             }; script-src 'nonce-${nonce}';">
-                <script src="${scriptUri}"></script>
                 <link rel="stylesheet" href="${styleUri}">
                 <link rel="stylesheet" href="${stylesResetUri}">
                 <link rel="stylesheet" href="${stylesMainUri}">
@@ -196,11 +195,18 @@ export class AchievementPanel {
                 </script>
 			    </head>
                 <body>
-                    <h1 align="center">Achievements</h1>
+                    <h1 align="center" id="heading">Achievements</h1>
+                    <hr>
                     <br><br>
                     <div class="achievements">${achievementsInText}</div>
+                    <p class="accomplishedAchievements">
+                    
+                    <a class="count✔️">${accomplishedAchievements(achievements).length}</a>/<a class="countAll">${achievements.length}</a>
+                    </p>
                 </body>
                 <footer id="footer" align="center">If you want to support me, you can <a href="https://www.buymeacoffee.com/Tchibo">buy me a coffee</a> ☕ <br></footer>
+                <script src="${scriptUri}" nonce="${nonce}">
+                </script>
 			    </html>`;
     }
 }
